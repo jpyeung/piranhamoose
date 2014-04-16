707dea1a7f481aeb65cc789ede9cc4d41a6934e0
Adam Yedidia <adamyedidia@gmail.com>
2014-04-15 23:06:34 -0400

    made the form work

--- inline.js
+++ inline.js
@@ -197,6 +197,41 @@ $(document).ready(function() {
     span.parent().find('button.not-editing').show();
     span.parent().find('button.editing').hide();
   }
+  
+  resorts = ["snowy-peaks", "mountainview", "barren-snowscape"];
+  
+  // The below is a function to attach a listener to the appropriate choice button in the resort form,
+  // given the name of the resort form.
+  var makeResortListener = function(resort) {
+    $("#" + resort + "-button").click(function() {
+	
+	  console.log("" + resort + "-name");
+	
+	  document.getElementById("resort-name").innerHTML = 
+		document.getElementById("" + resort + "-name").innerHTML;
+
+      document.getElementById("resort-address").innerHTML = 
+		document.getElementById("" + resort + "-address").innerHTML;
+	  
+	  document.getElementById("resort-difficulty").innerHTML = 
+		document.getElementById("" + resort + "-difficulty").innerHTML;
+	  
+	  document.getElementById("resort-price").innerHTML = 
+		document.getElementById("" + resort + "-price").innerHTML;
+	  
+	  document.getElementById("ski-patrol-number").innerHTML = 
+		document.getElementById("" + resort + "-ski-patrol-number").innerHTML;
+		
+	  document.getElementById("resort-distance").innerHTML = 
+	    document.getElementById("" + resort + "-distance").innerHTML;
+    });
+  }
+  
+  // This for loop iterates over all the resorts in the form and gives all their choice buttons 
+  // listeners, so that this process doesn't have to be done by hand.
+  for (var i=0; i<resorts.length; i++) {
+	makeResortListener(resorts[i]);
+  }
 
   // edit buttons
   $('#edit-leaving-at').click(function() {
