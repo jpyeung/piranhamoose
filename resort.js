2bb8cd799a643f107639f39d0b94525b54530b44
James Yeung <jpyeung@mit.edu>
2014-04-15 20:36:22 -0400

    index.js renamed to resort.js, old modals removed.

---
+++ resort.js
@@ -0,0 +1,20 @@
+// This allows the Javascript code inside this block to only run when the page
+// has finished loading in the browser.
+
+$(document).ready(function() {
+	
+	var changeResort = function(){
+	};
+
+	$("#snowy-peaks-button").click(function(evt) {
+		changeResort();
+	});
+
+	$("#mountainview-button").click(function(evt) {
+		changeResort();
+	});
+
+	$("#barren-snowscape-button").click(function(evt) {
+		changeResort();
+	});
+});
