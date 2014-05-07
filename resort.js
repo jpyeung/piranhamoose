c43971adf257f8cfc697c066f23054e79a186270
Stacey Terman <seterman@mit.edu>
2014-05-07 15:22:24 -0400

    Fix indentation in resort.js

--- resort.js
+++ resort.js
@@ -1,50 +1,47 @@
-// This allows the Javascript code inside this block to only run when the page
-// has finished loading in the browser.
-
 $(document).ready(function() {
-	resorts = ["blue-hills", "nashoba-valley", "boston-ski", "ski-bradford", "weston", "wachusett-mountain", "ski-ward"];
+  resorts = ["blue-hills", "nashoba-valley", "boston-ski", "ski-bradford", "weston", "wachusett-mountain", "ski-ward"];
 
-	// The below is a function to attach a listener to the appropriate choice button in the resort form,
-	// given the name of the resort form.
-	var makeResortListener = function(resort) {
-        $("#" + resort).css('cursor','pointer');
+  // The below is a function to attach a listener to the appropriate choice button in the resort form,
+  // given the name of the resort form.
+  var makeResortListener = function(resort) {
+    $("#" + resort).css('cursor','pointer');
 
-        $("#" + resort).hover(function() {
-            this.style.backgroundColor = '#d9edf7';
-        }, function() {
-            this.style.backgroundColor = 'white';
-        });
+    $("#" + resort).hover(function() {
+        this.style.backgroundColor = '#d9edf7';
+    }, function() {
+        this.style.backgroundColor = 'white';
+    });
 
-		$("#" + resort).click(function() {
-	
-	    document.getElementById("resort-name").innerHTML = 
-		  document.getElementById("" + resort + "-name").innerHTML;
-
-        document.getElementById("resort-address").innerHTML = 
-		  document.getElementById("" + resort + "-address").innerHTML;
-	  
-	    document.getElementById("resort-difficulty").innerHTML = 
-		  document.getElementById("" + resort + "-difficulty").innerHTML;
-	  
-	    document.getElementById("resort-price").innerHTML = 
-		  document.getElementById("" + resort + "-price").innerHTML;
-	  
-	    document.getElementById("resort-phone-number").innerHTML = 
-		  document.getElementById("" + resort + "-phone-number").innerHTML;
-
-        document.getElementById("resort-website").innerHTML = 
-          document.getElementById("" + resort + "-website").innerHTML;
-
-        $('#myModal').modal('hide');
-        showLocation();
-        var address = document.getElementById("resort-address").innerHTML;
-        updateWeatherFromLocation(address.substring(address.length - 6));
-    	});
-  	}
+    $("#" + resort).click(function() {
   
-  	// This for loop iterates over all the resorts in the form and gives all their choice buttons 
-  	// listeners, so that this process doesn't have to be done by hand.
-  	for (var i=0; i<resorts.length; i++) {
-		makeResortListener(resorts[i]);
-  	}
+      document.getElementById("resort-name").innerHTML = 
+        document.getElementById("" + resort + "-name").innerHTML;
+
+      document.getElementById("resort-address").innerHTML = 
+        document.getElementById("" + resort + "-address").innerHTML;
+    
+      document.getElementById("resort-difficulty").innerHTML = 
+        document.getElementById("" + resort + "-difficulty").innerHTML;
+    
+      document.getElementById("resort-price").innerHTML = 
+        document.getElementById("" + resort + "-price").innerHTML;
+    
+      document.getElementById("resort-phone-number").innerHTML = 
+        document.getElementById("" + resort + "-phone-number").innerHTML;
+
+      document.getElementById("resort-website").innerHTML = 
+        document.getElementById("" + resort + "-website").innerHTML;
+
+      $('#myModal').modal('hide');
+      showLocation();
+      var address = document.getElementById("resort-address").innerHTML;
+      updateWeatherFromLocation(address.substring(address.length - 6));
+    });
+  }
+  
+  // This for loop iterates over all the resorts in the form and gives all their choice buttons 
+  // listeners, so that this process doesn't have to be done by hand.
+  for (var i=0; i<resorts.length; i++) {
+    makeResortListener(resorts[i]);
+  }
 });
